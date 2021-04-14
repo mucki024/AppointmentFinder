213778cf74d6e4bed5dd716df8110fd7ccf21e2f
mucki024 <75478807+mucki024@users.noreply.github.com>
2021-04-14 07:59:38 +0200

    ts statt js?

---
+++ frontend/js/script.ts
@@ -0,0 +1,21 @@
+//Starting point for JQuery init
+$(document).ready(function () {
+    loaddata("Doe");
+});
+
+function loaddata(searchterm :string) {
+
+    $.ajax({
+        type: "GET",
+        url: "../backend/serviceHandler.php",
+        cache: false,
+        data: {method: "queryPersons", param: searchterm},
+        dataType: "json",
+        success: function (response) {
+            console.log(response);
+            //$("#noOfentries").val(response.length);
+            //$("#searchResult").show(1000).delay(1000).hide(1000);
+        }
+        
+    });
+}
